8cea8342ed07ff8f08e84cf43a5ee68cbd5f483d
Spencer Elliott <me@elliottsj.com>
2021-08-11 15:32:28 -0400

    fix(tests): fix tests

--- test/generate.js
+++ test/generate.js
@@ -2,9 +2,15 @@ import fs from 'fs';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
+const writeFile = fs.promises.writeFile;
+const mkdir = fs.promises.mkdir;
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-fs.writeFileSync(path.join(__dirname, 'generated.txt'), 'hello\nworld');
-fs.writeFileSync(path.join(__dirname, 'generated', 'hello.txt'), 'hello');
-fs.writeFileSync(path.join(__dirname, 'generated', 'world.txt'), 'world');
+(async () => {
+  await writeFile(path.join(__dirname, 'generated.txt'), 'hello\nworld');
+  await mkdir(path.join(__dirname, 'generated'), { recursive: true });
+  await writeFile(path.join(__dirname, 'generated', 'hello.txt'), 'hello');
+  await writeFile(path.join(__dirname, 'generated', 'world.txt'), 'world');
+})();
